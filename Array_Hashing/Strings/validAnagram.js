65599626aca814851ea652d7c4e73e8f5b6139bf
Mashood Ali <mashoodali219@gmail.com>
2023-07-16 23:12:04 +0500

    changes in valid anagram problem

--- Array_Hashing/Strings/validAnagram.js
+++ Array_Hashing/Strings/validAnagram.js
@@ -11,7 +11,7 @@ Input: s = "anagram", t = "nagaram"
 Output: true
  */
 
-//? solution
+//? solution1
 
 /**
  * @param {string} s
@@ -20,4 +20,25 @@ Output: true
  */
 var isAnagram = function (s, t) {
   return s.split("").sort().join("") === t.split("").sort().join("");
+};
+
+//? solution2
+
+var isAnagram = function (s, t) {
+  let map = new Map();
+  for (let c of s) {
+    map.set(c, (map.get(c) | 0) + 1);
+  }
+  for (let d of t) {
+    if (map.has(d)) {
+      map.set(d, map.get(d) - 1);
+      if (map.get(d) === 0) {
+        map.delete(d);
+      }
+    } else {
+      return false;
+    }
+  }
+  if (map.size === 0) return true;
+  else return false;
 };
